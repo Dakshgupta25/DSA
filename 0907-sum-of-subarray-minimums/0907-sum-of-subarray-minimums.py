43c5e4da6351f9607aed31e629328a8f21a77037
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-03 15:30:59 +0530

    Time: 875 ms (30.11%), Space: 16.2 MB (72.21%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,16 +1,41 @@
-class Solution(object):
-    def sumSubarrayMins(self, A):
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
-        A = [0]+A
-        result = [0]*len(A)
-        stack = [0]
-        for i in range(len(A)):
-            while A[stack[-1]] > A[i]:
-                stack.pop() 
-            j = stack[-1]
-            result[i] = result[j] + (i-j)*A[i]
+
+class Solution:
+    def sumSubarrayMins(self, arr):
+        MOD = 10**9 + 7
+        n = len(arr)
+
+        prev = [-1] * n
+        nxt = [n] * n
+
+        stack = []
+
+        # Previous Smaller
+        for i in range(n):
+            while stack and arr[stack[-1]] > arr[i]:
+                stack.pop()
+
+            if stack:
+                prev[i] = stack[-1]
+
             stack.append(i)
-        return sum(result) % (10**9+7)
+
+        stack = []
+
+        # Next Smaller or Equal
+        for i in range(n - 1, -1, -1):
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+
+            if stack:
+                nxt[i] = stack[-1]
+
+            stack.append(i)
+
+        ans = 0
+
+        for i in range(n):
+            left = i - prev[i]
+            right = nxt[i] - i
+            ans = (ans + arr[i] * left * right) % MOD
+
+        return ans
