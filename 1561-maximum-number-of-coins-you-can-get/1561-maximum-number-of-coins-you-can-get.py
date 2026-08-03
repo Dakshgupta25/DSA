d7c0d042a81e838bfa61b789bbe83485a4bb1338
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-03 14:23:26 +0530

    Time: 106 ms (59.65%), Space: 19 MB (42.98%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type piles: List[int]
         :rtype: int
         """
-        piles.sort(reverse=True)
+        piles.sort()
         n=len(piles)
         
         res=0
-        for i in range(0,n-n//3,2):
-            res+=piles[i+1]
+        for i in range(n//3,n,2):
+            res+=piles[i]
         return res
