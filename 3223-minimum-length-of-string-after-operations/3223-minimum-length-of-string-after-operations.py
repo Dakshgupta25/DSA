dadf8ad0fa71edc5a68caf15c145678e968fda91
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-04 22:38:55 +0530

    Time: 107 ms (100%), Space: 13.7 MB (66.67%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,14 +1,4 @@
 class Solution(object):
     def minimumLength(self, s):
-        freq = [0] * 26
-        n = len(s)
-
-        for ch in s:
-            idx = ord(ch) - ord('a')
-            freq[idx] += 1
-
-            if freq[idx] == 3:
-                freq[idx] -= 2
-                n -= 2
-
-        return n
+        a=[2 if (s.count(x))%2==0 else 1  for x in list(set(s))]
+        return sum(a)        
