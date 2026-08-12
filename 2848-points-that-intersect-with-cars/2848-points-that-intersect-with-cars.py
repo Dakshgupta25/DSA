48799706611afa4908d088118a4da78a5696caa2
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-12 10:54:58 +0530

    Time: 4 ms (84.54%), Space: 12.5 MB (24.74%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[List[int]]
         :rtype: int
         """
-        x=set()
-        for i in nums:
-            for j in range(i[0],i[1]+1):
-                if j not in x:
-                    x.add(j)
-        return len(list(x))
+        nums.sort(key = lambda x:x[0])
+        start  = nums[0][0]
+        end  = nums[0][1]
+        ans = 0
+        for  x in nums[1::]:
+            if end < x[0]:
+                ans += (end - start + 1) 
+                start = x[0]
+
+            end = max(end, x[1])
+        ans += (end - start + 1) 
+        return ans
