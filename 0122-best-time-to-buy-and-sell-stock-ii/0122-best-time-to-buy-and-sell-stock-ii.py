d03d195239ef0ebe86caa11b0d14dc9cb5f11ecf
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-13 16:13:39 +0530

    Time: 0 ms (100%), Space: 13.8 MB (36.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        ans=0
-        cur=prices[0]
+        profit=0
         for i in range(1,len(prices)):
-            if prices[i]<prices[i-1]:
-                ans+=max(0,prices[i-1]-cur)
-                cur=prices[i]
-        ans+=max(0,prices[-1]-cur)
-        return ans
+            if prices[i]>prices[i-1]:
+                profit=profit+(prices[i]-prices[i-1])
+        return profit
