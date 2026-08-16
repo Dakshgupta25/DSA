724d9fefc2caa960c9313204c5b12c2b03b41826
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-16 20:56:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (58.8%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,9 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        even=n*(n+1)
-        odd=n*n
-
-        while odd:
-            even,odd=odd,even%odd
-        return even
+        return n
