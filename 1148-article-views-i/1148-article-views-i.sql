245f094c7a22f09d997550ef60ac6ae2149cc99e
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-08-18 22:22:45 +0530

    Time: 509 ms (22.65%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,6 @@
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
-ORDER BY author_id;
+
+select author_id AS id
+from Views
+where author_id = viewer_id
+GROUP BY author_id
+ORDER BY id
