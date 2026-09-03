110de334e61dac159c204ec3f111f21018b5aae0
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-09-03 11:30:46 +0530

    Time: 6 ms (5.95%), Space: 12.5 MB (34.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -10,31 +10,22 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-
         if not strs:
             return ""
-
-        # Build Trie
-        root = TrieNode()
-
+        
+        root=TrieNode()
         for word in strs:
-            node = root
-
+            node=root
             for ch in word:
                 if ch not in node.children:
-                    node.children[ch] = TrieNode()
+                    node.children[ch]=TrieNode()
+                node=node.children[ch]
+            node.isEnd=True
 
-                node = node.children[ch]
+        prefix=""
 
-            node.isEnd = True
-
-        # Find common prefix
-        prefix = ""
-        node = root
-
-        while len(node.children) == 1 and not node.isEnd:
-            ch = next(iter(node.children))
-            prefix += ch
-            node = node.children[ch]
-
+        while len(root.children)==1 and root.isEnd==False:
+            ch=list(root.children)[0]
+            prefix+= ch
+            root=root.children[ch]
         return prefix
