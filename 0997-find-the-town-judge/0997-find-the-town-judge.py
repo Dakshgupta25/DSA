49f86d30affb9a786a360e18fe5f220d299782d6
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-09-08 09:52:36 +0530

    Time: 19 ms (40.55%), Space: 16 MB (75.03%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -5,15 +5,13 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
-        indegree = [0] * (n + 1)
-        outdegree = [0] * (n + 1)
+        if len(trust) == 0 and n == 1: 
+            return 1
+        count = [0] * (n + 1)
+        for person in trust:
+            count[person[0]] -= 1
+            count[person[1]] += 1
 
-        for a, b in trust:
-            outdegree[a] += 1
-            indegree[b] += 1
-
-        for person in range(1, n + 1):
-            if indegree[person] == n - 1 and outdegree[person] == 0:
-                return person
-
+        for person in range(len(count)):
+            if count[person] == n - 1: return person
         return -1
