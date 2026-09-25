059409760dd07eb009431029a5437a24c458bf78
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-09-25 16:30:28 +0530

    Time: 11 ms (35.9%), Space: 15.4 MB (32.84%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,43 +1,26 @@
 class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
+    def canFinish(self, n, pre):
+        graph=[[] for _ in range(n)]
 
-        graph = [[] for _ in range(numCourses)]
+        for i,j in pre:
+            graph[i].append(j)
+        
+        state=[0]*n
 
-        # Build graph
-        for course, prerequisite in prerequisites:
-            graph[course].append(prerequisite)
-
-        # 0 = not visited
-        # 1 = currently visiting
-        # 2 = completely processed
-        state = [0] * numCourses
-
-        def dfs(course):
-
-            # Cycle found
-            if state[course] == 1:
+        def dfs(cor):
+            if state[cor]==1:
                 return False
-
-            # Already checked this course
-            if state[course] == 2:
+            if state[cor]==2:
                 return True
+            state[cor]=1
 
-            # Currently exploring
-            state[course] = 1
-
-            for prerequisite in graph[course]:
-
-                if not dfs(prerequisite):
+            for i in graph[cor]:
+                if not dfs(i):
                     return False
-
-            # Finished exploring this course
-            state[course] = 2
-
+            state[cor]=2
             return True
-
-        for course in range(numCourses):
-
-            if not dfs(course):
+        
+        for i in range(n):
+            if not dfs(i):
                 return False
-
         return True
